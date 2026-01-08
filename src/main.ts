40dc2bd853b50d5188c2ad89112ccae473606a00
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2026-01-08 22:05:12 +0300

    первый коммит на tyoescript

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,26 @@ console.log(a);
 
 const b: number = 22;
 console.log(a + b);
+const a1: A = {
+  age: 10,
+  nick: 'mynick',
+  info: [1, 100],
+  photo: 'string',
+};
+
+const a2: A = {
+  age: '10 years',
+  nick: '',
+  info: { id: 100 },
+  photo: null,
+};
+
+const a3: A = {
+  nick: '       ',
+  info: ['secret', 'key'],
+};
+
+const a4: A = {
+  nick: '',
+  info: {},
+};
